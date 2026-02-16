31a8fb45c6ee6d8bf419791abd2f1209be6518b3
Ethan <43922849+ethanppl@users.noreply.github.com>
2026-02-17 00:37:30 +0800

    [dvd] Add variations.

--- js/index.js
+++ js/index.js
@@ -4,13 +4,33 @@ const ANIMATION_INTERVAL_MS = 10;
 const SPEED = 50;
 const PADDING = 3;
 
+const VARIATIONS = [
+  "For Fun!",
+  "<i>DVD</i>",
+  "Pam claims that she saw it.",
+  "I saw it, and it was amazing!",
+  "That was so awesome.",
+  "Will it hit?",
+  "Pure luck. Pure joy.",
+  "It was only a matter of time.",
+  "Be patient!",
+  "Eventually...",
+  "Barely missed?",
+];
+
+const SATURATION = "55%"
+const LIGHTNESS = "40%"
+
 function main() {
-  let element = document.getElementById(BOUNCY_ELEMENT_ID);
+  const element = document.getElementById(BOUNCY_ELEMENT_ID);
 
   if (element == null) {
     console.warn(`Cannot find element with ID ${BOUNCY_ELEMENT_ID}, exiting.`);
     return;
   }
+
+  const randIdx = Math.round(Math.random() * VARIATIONS.length) % VARIATIONS.length;
+  element.innerHTML = VARIATIONS[randIdx];
 
   let elementWidth = element.offsetWidth;
   let elementHeight = element.offsetHeight;
@@ -60,6 +80,12 @@ function main() {
   }
 }
 
+function randomizeColour() {
+  const element = document.getElementById(BOUNCY_ELEMENT_ID);
+  const hue = Math.round(Math.random() * 360) % 360;
+  element.style.color = `hsl(${hue}, ${SATURATION}, ${LIGHTNESS})`;
+}
+
 function pingPong(min, max, current, inputDelta, isPositiveDirection) {
   let output = current;
   let direction = isPositiveDirection;
@@ -80,11 +106,13 @@ function pingPong(min, max, current, inputDelta, isPositiveDirection) {
   if (output < min) {
     output = min - output;
     direction = !direction;
+    randomizeColour();
   }
 
   if (output > max) {
     output = max - (output - max);
     direction = !direction;
+    randomizeColour();
   }
 
   return [output, direction];
